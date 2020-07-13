18e5b3dc440ddfaf5cf0b28bffe5e88613389169
yunshangkeiji <yunshangkeiji@dev>
2020-07-13 22:38:31 +0800

    :disabled="loading"

--- ruoyi-ui/src/views/live/option/components/TabPane.vue
+++ ruoyi-ui/src/views/live/option/components/TabPane.vue
@@ -1,22 +1,24 @@
 <template>
-  <el-form label-width="100px">
+  <el-form label-width="100px" @submit.native.prevent="apiUpdate">
     <el-form-item v-for="(oRow, sName) in settings" :key="sName" :label="oRow.title" :prop="sName">
       <el-input
         v-if="oRow.type==='text'"
         v-model="oRow.value"
         :placeholder="'请输入【'+oRow.title+'】'"
+        :disabled="loading"
       />
       <el-input
         v-if="oRow.type==='textarea'"
         v-model="oRow.value"
         type="textarea"
         :rows="oRow.rows"
+        :disabled="loading"
       ></el-input>
-      <el-radio-group v-else-if="oRow.type==='radio'" v-model="oRow.value">
+      <el-radio-group v-else-if="oRow.type==='radio'" v-model="oRow.value" :disabled="loading">
         <el-radio v-for="(value,name) in oRow.option" :key="name" :label="name">{{value}}</el-radio>
       </el-radio-group>
     </el-form-item>
-    <el-button type="primary" @click="apiUpdate">保存设置</el-button>
+    <el-button type="primary" :disabled="loading" @click="apiUpdate">保存设置</el-button>
   </el-form>
 </template>
 
@@ -54,7 +56,7 @@ export default {
       });
     },
     /* 保存设置按钮 */
-    apiUpdate: function() {
+    apiUpdate() {
       const settings = {};
       for (var sName in this.settings) {
         settings[sName] = this.settings[sName].value;
