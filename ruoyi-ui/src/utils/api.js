29ee50d9eb4ac1084ea73f72771a16613465dca1
yunshangkeiji <yunshangkeiji@dev>
2020-07-29 18:58:32 +0800

    if (mData === undefined)

--- ruoyi-ui/src/utils/api.js
+++ ruoyi-ui/src/utils/api.js
@@ -1,14 +1,18 @@
 import request from "@/utils/request";
 
 export function api(sPath, mData) {
+  if (mData === undefined) {
+    mData = {}
+  }
   const aPath = sPath.split('/');
   const sCategory = aPath[0];
   const aPath1 = aPath[1].split(':');
   const sObject = aPath1[0];
-  const sMethod = aPath1[1];
   const option = {};
   option.url = "/admin/" + sCategory + "/" + sObject;
-  option.method = sMethod;
+  if (aPath1[1]) {
+    option.method = aPath1[1];
+  }
   option.data = mData;
   return request(option);
 }
