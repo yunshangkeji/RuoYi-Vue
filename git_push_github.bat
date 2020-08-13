4925285b03eea44e98b1213a1b0c8bbe0f6287d6
yskj <dev@yskj.local>
2020-08-13 20:21:06 +0800

    path

--- git_push_github.bat
+++ git_push_github.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+set path=%path%;D:\Users\asheng\Programs\PortableGit\bin
 ECHO =========git push github master============
 git remote add github https://github.com/yunshangkeji/RuoYi-Vue
 git pull github master
