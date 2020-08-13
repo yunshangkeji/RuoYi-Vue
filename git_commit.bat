5836b7052929475ecd4dd3ebdfd8c7d7241df93f
yskj <dev@yskj.local>
2020-08-13 19:04:12 +0800

    add

---
+++ git_commit.bat
@@ -0,0 +1,25 @@
+@ECHO OFF
+git config --global user.email "dev@yskj.local"
+git config --global user.name "yskj"
+git config --global core.safecrlf warn
+git config --global core.autocrlf true
+git remote rm origin
+git remote add origin https://gitee.com/yunshangkeji1/RuoYi-Vue
+ECHO =========git diff============
+git diff
+ECHO =========git pull ^&^& git commit============
+PAUSE
+git pull origin master
+git commit
+ECHO =========git add *============
+PAUSE
+git add --all
+ECHO =========git commit============
+PAUSE
+git commit
+ECHO =========git push origin master============
+PAUSE
+git push origin master
+ECHO =========EXIT============
+PAUSE
+EXIT
